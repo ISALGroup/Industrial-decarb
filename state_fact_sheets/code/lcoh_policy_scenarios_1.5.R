172315e77bcac60448195c45416dac57d0e7344f
nmariano9 <nmariano9@gmail.com>
2025-09-03 17:01:12 -0700

    Webtool work

--- state_fact_sheets/code/lcoh_policy_scenarios_1.5.R
+++ state_fact_sheets/code/lcoh_policy_scenarios_1.5.R
@@ -22,7 +22,7 @@ tech_input_df <-
   read_excel(glue("LCOH modelling/output/longform_{state}.xlsx")) %>%
   mutate(facility_name = tolower(facility_name), 
          baseline_co2e_emissions = elec_ghg_emissions + noelec_ghg_emissions) %>% 
-  select(-1, -facility_id, -opex) #removing for now til i get complete results from antoine
+  select(-1, -opex) #removing for now til i get complete results from antoine
 
 # TEMPORARY: natgas capex calculation
 natgas_best <- 
@@ -42,57 +42,57 @@ tech_input_df <-
   bind_rows(natgas_best)
 
 
-# # Pull in lat and long from rlps file
-# facility_lat_long <- 
-#   read_excel("state_fact_sheets/data/raw/rlps_ghg_emitter_subpart_w_NAICS.xlsx") %>% 
-#   select(facility_id, latitude, longitude) %>% 
-#   distinct(facility_id, .keep_all = TRUE)
-# 
-# # Pull in facility info, this file doesn't have lat and long... (maybe add in the future)
-# facility_info <- 
-#   read_excel("state_fact_sheets/data/raw/Facility_and_Unit_Emissions_Database_2023_v3.xlsx", sheet = 2) %>% 
-#   clean_names() %>% 
-#   rename(naics_code = primary_naics) %>% 
-#   rename(naics_description = naics_title) %>% 
-#   mutate(facility_name = tolower(facility_name)) %>% 
-#   select(facility_name, facility_id, naics_code, naics_description, county_fips, subregion) %>% 
-#   inner_join(facility_lat_long, by = "facility_id") %>% 
-#   mutate(
-#     sector = case_when(
-#       str_starts(naics_code, "311") | str_starts(naics_code, "312") ~ "Food & Beverage",
-#       str_starts(naics_code, "325") ~ "Chemicals",
-#       str_starts(naics_code, "322") ~ "Pulp & Paper",
-#       TRUE ~ "Other Manufacturing"
-#     )
-#   )
-# 
-# # eGRID data
-# egrid_df <- 
-#   read_excel("state_fact_sheets/data/raw/egrid_summary_tables_rev2.xlsx", sheet = 2) %>% 
-#   clean_names() %>% 
-#   mutate(co2e_kg_kwh = co2e * 0.000453592) %>% 
-#   mutate(ch4_kg_kwh = ch4 * 0.000453592) %>% 
-#   mutate(n2o_kg_kwh = n2o * 0.000453592) %>% 
-#   mutate(nox_kg_kwh = annual_n_ox * 0.000453592) %>% 
-#   mutate(so2_kg_kwh = so2 * 0.000453592) %>% 
-#   mutate(pm25_kg_kwh = pm_2_5 * 0.000453592) %>% 
-#   rename(subregion = e_grid_subregion_acronym) %>% 
-#   select(subregion, co2e_kg_kwh, nox_kg_kwh, so2_kg_kwh, pm25_kg_kwh)
-# 
-# # Resource mix 
-# grid_mix_df <- 
-#   read_excel("state_fact_sheets/data/raw/egrid_summary_tables_rev2.xlsx", sheet = 3) %>%
-#   clean_names() %>%
-#   mutate(
-#     current_fossil_share = coal + oil + gas + other_fossil,
-#     current_clean_share = hydro + biomass + wind + solar + nuclear #removed geothermal for now due to E, close to 0
-#   ) %>%
-#   select(subregion = e_grid_subregion_acronym, current_fossil_share, current_clean_share)
+# Pull in lat and long from rlps file
+facility_lat_long <-
+  read_excel("state_fact_sheets/data/raw/rlps_ghg_emitter_subpart_w_NAICS.xlsx") %>%
+  select(facility_id, latitude, longitude) %>%
+  distinct(facility_id, .keep_all = TRUE)
+
+# Pull in facility info, this file doesn't have lat and long... (maybe add in the future)
+facility_info <-
+  read_excel("state_fact_sheets/data/raw/Facility_and_Unit_Emissions_Database_2023_v3.xlsx", sheet = 2) %>%
+  clean_names() %>%
+  rename(naics_code = primary_naics) %>%
+  rename(naics_description = naics_title) %>%
+  mutate(facility_name = tolower(facility_name)) %>%
+  select(facility_name, facility_id, naics_code, naics_description, county_fips, subregion) %>%
+  inner_join(facility_lat_long, by = "facility_id") %>%
+  mutate(
+    sector = case_when(
+      str_starts(naics_code, "311") | str_starts(naics_code, "312") ~ "Food & Beverage",
+      str_starts(naics_code, "325") ~ "Chemicals",
+      str_starts(naics_code, "322") ~ "Pulp & Paper",
+      TRUE ~ "Other Manufacturing"
+    )
+  )
+
+# eGRID data
+egrid_df <-
+  read_excel("state_fact_sheets/data/raw/egrid_summary_tables_rev2.xlsx", sheet = 2) %>%
+  clean_names() %>%
+  mutate(co2e_kg_kwh = co2e * 0.000453592) %>%
+  mutate(ch4_kg_kwh = ch4 * 0.000453592) %>%
+  mutate(n2o_kg_kwh = n2o * 0.000453592) %>%
+  mutate(nox_kg_kwh = annual_n_ox * 0.000453592) %>%
+  mutate(so2_kg_kwh = so2 * 0.000453592) %>%
+  mutate(pm25_kg_kwh = pm_2_5 * 0.000453592) %>%
+  rename(subregion = e_grid_subregion_acronym) %>%
+  select(subregion, co2e_kg_kwh, nox_kg_kwh, so2_kg_kwh, pm25_kg_kwh)
+
+# Resource mix
+grid_mix_df <-
+  read_excel("state_fact_sheets/data/raw/egrid_summary_tables_rev2.xlsx", sheet = 3) %>%
+  clean_names() %>%
+  mutate(
+    current_fossil_share = coal + oil + gas + other_fossil,
+    current_clean_share = hydro + biomass + wind + solar + nuclear #removed geothermal for now due to E, close to 0
+  ) %>%
+  select(subregion = e_grid_subregion_acronym, current_fossil_share, current_clean_share)
 
 # --- Merge Inputs ---
 tech_combined_df <- 
   tech_input_df %>%
-  left_join(facility_info, by = "facility_name") %>%
+  left_join(facility_info, by = "facility_id") %>%
   left_join(egrid_df, by = "subregion") 
 
 #### EMISSIONS CALCULATION #### 
@@ -111,22 +111,22 @@ facility_grid_long_df <-
     scaled_co2e_factor = if_else(
       is.na(grid_clean_pct_num),
       co2e_kg_kwh,
-      co2e_kg_kwh * ((1 - grid_clean_pct_num) / current_fossil_share)
+      co2e_kg_kwh * (1 - grid_clean_pct_num)
     ),
     scaled_nox_factor = if_else(
       is.na(grid_clean_pct_num),
       nox_kg_kwh,
-      nox_kg_kwh * ((1 - grid_clean_pct_num) / current_fossil_share)
+      nox_kg_kwh * (1 - grid_clean_pct_num)
     ),
     scaled_so2_factor = if_else(
       is.na(grid_clean_pct_num),
       so2_kg_kwh,
-      so2_kg_kwh * ((1 - grid_clean_pct_num) / current_fossil_share)
+      so2_kg_kwh * (1 - grid_clean_pct_num) 
     ),
     scaled_pm25_factor = if_else(
       is.na(grid_clean_pct_num),
       pm25_kg_kwh,
-      pm25_kg_kwh * ((1 - grid_clean_pct_num) / current_fossil_share)
+      pm25_kg_kwh * (1 - grid_clean_pct_num) 
     ))
 
 facility_emissions_no_baseline <- 
--- webtool/code/webtool_functions.R
+++ webtool/code/webtool_functions.R
@@ -8,55 +8,64 @@ library(glue)
 
 tech_combined_df <- read_excel('file_path')
 
-
 #### EMISSIONS FUNCTION #### 
-# This takes user inputs & facility-level data to produce a facility-level emissions estimate 
-# Data can then be averaged by sector, technology scenario, and/or state to produce the visual layer
+# This takes user inputs & facility-level data to produce a facility-level emissions estimate.  
+
+# You could apply this inside a dplyr pipe, e.g.:
+## 1. Filter dataset, per user input 
+## 2. Use function inside mutate() to calculate emissions outcome for each observation
+## 3a. Sum across all observations to get the total emissions for a given scenario (top bar). 
+## 3b. Group_by state/county to get the results for a pop-up box when a user clicks on a geographic area.
+
+# Filtering variables on the emissions dataset: 
+## tech_scenario --> Electrification scenario 
+## fuels --> Fuel type at the facility 
+## county_fips --> County
+## state --> State 
+## naics_code / sector --> Sector 
+## pollutant_type --> co2e, nox, so2, pm25 
 
 emissions_func <- 
-  function(df, 
+  function(# From Emissions Dataset
+           grid_emissions_kg_kwh, # kg of emissions per kwh
+           change_in_electricity_demand_kwh, # how much electricity demand changes under electrification
            
            # User Inputs 
-           grid_clean_pct, 
-           pollutant, #co2e, nox, so2, or pm25
-           tech_scenario
-           #total_or_relative
+           tech_scenario, # Which electrification scenario are we looking at? (Baseline, E-Boiler, etc.)
+           grid_clean_pct, # What % cleaner is the grid? should take a value from 0-1
+           pollutant_type # For now, let's only do CO2e 
            ) {
-      
-    in_col  <- paste0(pollutant, "_kg_kwh")
 
-    df <- 
-      df %>%
-      # Scale grid emissions based on grid mix already present in tech_df
-      mutate(
-        scaled_emissions_factor := .data[[in_col]] * ((1 - grid_clean_pct) / current_fossil_share)
-      ) %>%
-      
-      # calculate change in 
+    # Scale the grid emissions (kg per kwh) to be more clean (or the same, depending on what the user selects). 
+    scaled_grid.e_factor <- grid_emissions_kg_kwh * (1-grid_clean_pct)
     
-  
-  # Non-baseline
-  facility_emissions_no_baseline <- facility_grid_df %>%
-    filter(tech_scenario != "Baseline") %>%
-    mutate(
-      emissions_total_t_co2e = (change_in_electricity_demand_kwh * scaled_co2e_factor) / 1000 + noelec_ghg_emissions,
-      emissions_change_kg_nox = change_in_electricity_demand_kwh * scaled_nox_factor,
-      emissions_change_kg_so2 = change_in_electricity_demand_kwh * scaled_so2_factor,
-      emissions_change_kg_pm25 = change_in_electricity_demand_kwh * scaled_pm25_factor
-    )
-  
-  # Baseline
-  facility_emissions_baseline <- facility_grid_df %>%
-    filter(str_detect(tech_scenario, "Baseline")) %>%
-    mutate(
-      emissions_total_t_co2e = baseline_co2e_emissions,
-      emissions_change_kg_nox = 0,
-      emissions_change_kg_so2 = 0,
-      emissions_change_kg_pm25 = 0
-    )
+    case_when(
+      # When pollutant is CO2e & we are looking at an electrification scenario... 
+      pollutant_type == 'co2e' & tech_scenario != "Baseline" ~ {
+        # emissions = the increase in emissions from the grid from electricity demand + non-electrifiable emissions at the facility 
+        emissions_total_t = (change_in_electricity_demand_kwh * scaled_grid.e_factor) / 1000 + noelec_ghg_emissions
+        
+        emissions_total_t
+      }, 
+      
+      # When pollutant is not CO2e... (ignore for now) 
+      tech_scenario != "Baseline" ~ {
+        emissions_change_kg = change_in_electricity_demand_kwh * scaled_grid.e_factor
+        
+        emissions_change_kg
+      }, 
+      
+      # When we are looking at the baseline scenario, just return the baseline emissions from GHGRP. 
+      tech_scenario == "Baseline" ~ {
+        baseline_co2e_emissions
+      })
   }
 
+
 #### COST FUNCTION ####
+
+# This takes user inputs & facility-level data to produce a facility-level "levelized cost of heat" (LCOH) estimate.  
+
 
 lcoh_func <- function(
   ## User inputs 
